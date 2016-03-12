3482473b2e81568feb51e92a1f43e466d429d2f0
Bryce Krah <krahbryce@yahoo.com>
2016-03-12 11:04:22 -0500

    set up REST routes for movies, and theaters, tested in postman all routes being hit correctly

--- routes/movies.js
+++ routes/movies.js
@@ -1,1 +1,26 @@
 'use strict'
+const express = require('express');
+const movies = express.Router();
+const bodyParser = require('body-parser');
+const db = require('./../db/pg');
+
+movies.route('/:movieID')
+  .get( (req,res)=>{
+    res.send("specific movie get route")
+  })
+  .put( (req,res)=>{
+    res.send("edit a specific movie")
+  })
+  .delete( (req,res)=>{
+    res.send("delete a specific movie")
+  })
+
+movies.route('/')
+  .get( (req,res)=>{
+    res.send("get all movies from a theater")
+  })
+  .post( (req,res)=>{
+    res.send("add a movie to a specific theater")
+  })
+
+  module.exports = movies;
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.port || 3000;
 
+const theaterRoutes = require( path.join(__dirname, '/routes/theaterRoutes'));
+const movieRoutes = require( path.join(__dirname, '/routes/movies'))
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
@@ -23,6 +26,10 @@ app.get('/', (req,res) => {
     res.render('pages/index');
 
 });
+
+app.use('/theaters', theaterRoutes);
+app.use('/theaters/:theaterID/movies', movieRoutes);
+
 
 // turn me on!
 app.listen(port, () => {
