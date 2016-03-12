4518fd0d9db6bbfab8b0fab800203d585ff8fc23
Bryce Krah <krahbryce@yahoo.com>
2016-03-12 11:22:55 -0500

    set up schema file

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE if EXISTS movies CASCADE;
+DROP TABLE if EXISTS theaters CASCADE;
+DROP TABLE if EXISTS xref CASCADE;
+
+CREATE TABLE theaters (
+  id SERIAL PRIMARY KEY UNIQUE,
+  name VARCHAR(200)
+);
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY UNIQUE,
+  title VARCHAR(200),
+  year VARCHAR(200),
+  rating VARCHAR(100),
+  director VARCHAR(200),
+  actors VARCHAR(2000),
+  plot VARCHAR(2000),
+  poster VARCHAR(2500)
+);
+
+CREATE TABLE xref (
+  movie_id INTEGER REFERENCES movies (id) ON DELETE CASCADE,
+  theater_id INTEGER REFERENCES theaters (id) ON DELETE CASCADE,
+  PRIMARY KEY (movie_id, theatre_id)
+);
