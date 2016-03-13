66522daa23e88346d87a1f7430a0fb230a1acc4f
Bryce Krah <krahbryce@yahoo.com>
2016-03-12 21:57:12 -0500

    search result component: gets result from omdb and displays to page, add button sends it to our DB, cancel brings you back to home page view

--- public/js/pad.js
+++ public/js/pad.js
@@ -1,14 +1,23 @@
-addMovie : function (newMovie) {
-  //i'm not entirely confident in this bit of code.
-  var passMovie = this.state.testObj.movie;//need to figure out what we want in state, and at what level. this is almost certainly incorrect.
-  var addNew = (data) => {
-    var newID = this.state.testObj.movie;
-    this.state.testObj.movie[newID] = data; //adds new Movie to textObj
-    // this.setState( { testObj : this.state.testObj }) //this entirely depends on state, so this won't work as is.
-  }
-  $.post('/OTHERrouteHERE', this.state.testObj.movie)
-    .done(
-      addNew
+'use strict'
+const React = require('react');
+
+
+const Test = React.createClass({
+  handleClick: function(event){
+    event.preventDefault();
+    console.log("clicked add movie button")
+  },
+
+  render: function(){
+    return(
+      <div>
+        <h1>{this.props.movieSearch.Title}</h1>
+        <h3>{this.props.movieSearch.Year}</h3>
+        <img src={this.props.movieSearch.Poster} />
+        <button onClick={this.handleClick} type="submit">Add</button>
+      </div>
     )
-},
+  }
 })
+
+module.exports = Test;
--- public/js/script.js
+++ public/js/script.js
@@ -1,88 +1,83 @@
 'use strict'
 const React = require('react');
 const ReactDOM = require('react-dom');
-const $ = require('jquery');
+const SearchOMDB = require('./searchomdb.js')
+const SearchResult = require('./searchresult.js')
 
 const App = React.createClass( {
   getInitialState : function () {
-    return {
-      testObj : {}
-    }
+    return (
+      {
+        movieSearch : {},
+        searching: false
+      }
+    )
   },
   componentDidMount : function () {
-    //get data from DB on render
-    $.get('/ROUTEgoesHERE')
-      .done( (data) => {
-        //this will likely need to be re-written
-        data.forEach( (el) => {
-          this.state.testObj[el.KEY_ID] = el;
-        })
-        // console.log(Object.keys.this.state.testObj);
-        this.setState( {testObj : this.state.testObj})
-      })
+    // //get data from DB on render
+    // $.get('/ROUTEgoesHERE')
+    //   .done( (data) => {
+    //     //this will likely need to be re-written
+    //     data.forEach( (el) => {
+    //       this.state.testObj[el.KEY_ID] = el;
+    //     })
+    //     // console.log(Object.keys.this.state.testObj);
+    //     this.setState( {testObj : this.state.testObj})
+    //   })
   },
-  addTheater : function (newTheater) {
-    //i'm not entirely confident in this bit of code.
-    var passTheater = this.state.testObj;
-    var addNew = (data) => {
-      var newID = this.state.testObj;
-      this.state.testObj[newID] = data; //adds new theater to textObj
-      this.setState( { testObj : this.state.testObj })
-    }
-    $.post('/OTHERrouteHERE', this.state.testObj)
-      .done(
-        addNew
-      )
+  // addTheater : function (newTheater) {
+  //   //i'm not entirely confident in this bit of code.
+  //   var passTheater = this.state.testObj;
+  //   var addNew = (data) => {
+  //     var newID = this.state.testObj;
+  //     this.state.testObj[newID] = data; //adds new theater to textObj
+  //     this.setState( { testObj : this.state.testObj })
+  //   }
+  //   $.post('/OTHERrouteHERE', this.state.testObj)
+  //     .done(
+  //       addNew
+  //     )
+  // },
+
+  searchForMovie: function(title){
+
+    $.get('http://www.omdbapi.com/?t='+title)
+    .done((data)=>{
+      this.state.movieSearch = data;
+      this.setState({movieSearch: this.state.movieSearch,
+                      searching: true})
+      console.log(this.state.movieSearch)
+    })
+
+},
+
+  cancelSearch: function(){
+  this.setState({searching: false})
+},
+
+  addMovie: function( newMovie ){
+    $.post('/theaters/1/movies', newMovie)
+    .done((data)=>{
+      console.log(data)
+    })
   },
-  //addMovie : copy from pad to here once we decide on nomenclature
-  //deleteTheater : //code
-  //deleteMovie : //code
+
+
   render : function () {
     //set any vars/operations we may need for render
 
-    return (
-      <div {/* App div inside container div, kind of redundant */} >
-        //the closing make more sense to the linter once we assign each component to a prop, but the linter can't read this well as is
-        <AllTheatersDisplay
-          {/* various props */}
+    let searchResultView =  <div>
+                              <SearchOMDB searchMovie={this.searchForMovie} />
+                              <SearchResult movieSearch={this.state.movieSearch} cancelSearch={this.cancelSearch} addMovie={this.addMovie}/>
+                            </div>
 
-          <OneTheaterDisplay
-            {/* props, including one to indicate which theater this is*/}
-            <MovieList
-              {/* props */}
-              <OneMovieDisplay
-                {/* props, including one to indicate which movie this is. title? playing times?*/}
-                 {/* OneMovieDisplay close */}
-                 {/* MovieList close */}
-             {/* OneTheaterDisplay close */}
-             {/* AllTheatersDisplay close */}
-          <CurrentMovieDisplay
-            {/* various props. poster, title, year, rating, director, plot (PTYRDP) might be condensible to a single element/prop */}
-            <PTYRDP
-              />
-            <ActorArrayOrList
-              />
-            <MagicButton type="edit"
-              /> //this type may need to take another form
-            <MagicButton type="delete"
-              /> //this type may need to take another form
-            />
-          <SearchArea
-            {/* props */}
-            <FormContainer
-              <TextInput
-                />
-              <SubmitButton
-                />
-              />
-            <ResultField
-              {/* props */}
-              <PTYP
-                />
-              <MagicButton type="submit"
-                /> //this type may need to take another form
-              />
-            />
+    let homeView =  <div>
+                      <SearchOMDB searchMovie={this.searchForMovie} />
+                    </div>
+
+    return (
+      <div>
+        {this.state.searching ? searchResultView : homeView}
       </div>
     )
   }
