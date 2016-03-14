c7f577ac5346027475ac3ec7bc6e660943705067
Patrick M <Patrick.M.Professional@gmail.com>
2016-03-13 21:19:10 -0400

    added barebones functions to add and edit
    this may delete bundle when you first pull, but that should get reassembled by the start script. haven't actually hooked edit and add up to the buttons, mostly cause i'm pretty sure they dont work.

--- public/js/movieView.js
+++ public/js/movieView.js
@@ -4,6 +4,37 @@ const React = require('react');
 const MovieView = React.createClass( {
 
 //need to pass button info to allow for flexibility. pass each button as subComponent1=
+
+  editMovie : function (newInfo) {
+    $.ajax({
+      method: 'PUT',
+      url: '/:movieID',
+      //do we need to put an href or action on the button to specify this URL?
+      data: newInfo, //new info should be the movie info grabbed from the page that we are editing? do we have an edit component? what is going onnnnnnn
+      //where do i specify db.editMovie ?
+    })
+      .done( (data) => {
+        console.log('editMovie .done in movieView was called');
+        //i'm 99.99% sure i need to do something here, but brain is out of juice.
+      })
+      .fail( (error) => {
+        console.log(error, 'editMovie in movieView had that error');
+      })
+  }, //end of editMovie
+  deleteMovie : function () {
+    $.ajax({
+      method: 'DELETE',
+      url: '/:movieID',
+      //how are we calling db.deleteMovie on this req.params.movieID
+    })
+      .done( (data) => {
+        console.log('deleteMovie .done in movieView was called');
+        //i'm 99.99% sure i need to do something here, but brain is out of juice.
+      })
+      .fail( (error) => {
+        console.log(error, 'deleteMovie in movieView had that error');
+      })
+  }, //end of deleteMovie
   render : function () {
 
     return (
